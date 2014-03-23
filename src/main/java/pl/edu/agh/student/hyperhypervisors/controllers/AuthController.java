a5c34bf92623b728d10f1646c24c8cc6fbc765ae
Artur Orzeł <artekorzel@gmail.com>
2014-03-23 12:18:21 +0100

    further configuration, maven-tomcat7-plugin, maven-site-plugin

---
+++ src/main/java/pl/edu/agh/student/hyperhypervisors/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.student.hyperhypervisors.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "/auth/login";
+    }
+}
---
+++ src/main/java/pl/edu/agh/student/hyperhypervisors/controllers/MainController.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.student.hyperhypervisors.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
